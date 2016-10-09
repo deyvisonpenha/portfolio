d8fa682c83e0f873cbb6021953d3016b6d64a802
deyvisonpenha <deyvisonpenha1@gmail.com>
2016-10-09 15:37:51 -0300

    primeira versao funcionando

---
+++ app/controllers/portfolio_controller.rb
@@ -0,0 +1,7 @@
+class PortfolioController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'portfolio/index'
+
+  get 'portfolio/login'
+
+  get 'portfolio/about'
+
   resources :invoices
+
+  root to: 'portfolio#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
