2fa07444dad4688fdfe88cc742f97bbb98007883
deyvisonpenha <deyvisonpenha1@gmail.com>
2017-01-16 13:34:56 -0300

    tudo funcionando em produção

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,10 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.enabled = true
+
+Rails.application.config.assets.compile = true
+
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf|jpg)\z/
+Rails.application.config.assets.precompile += %w( *.js )
+Rails.application.config.assets.precompile += %w( *.css )
